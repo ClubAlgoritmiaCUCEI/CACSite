74f2e1a8a5b700b3366914f7235ed4631ff5afc5
Franco1010 <franco.ramirez@alumno.udg.mx>
2020-02-01 23:49:21 -0600

    Month initial view in calendar

--- src/Views/calendar/index.js
+++ src/Views/calendar/index.js
@@ -14,7 +14,7 @@ const Calendar = () => {
         </div>
         <iframe
           className="cac_calendar_calendar"
-          src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FMexico_City&amp;src=Y2x1Yi5hbGdvcml0bWlhLmN1Y2VpQGdtYWlsLmNvbQ&amp;color=%2322AA99&amp;mode=WEEK&amp;hl=en&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;showTz=1&amp;title"
+          src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FMexico_City&amp;src=Y2x1Yi5hbGdvcml0bWlhLmN1Y2VpQGdtYWlsLmNvbQ&amp;color=%2322AA99&amp;mode=MONTH&amp;hl=en&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;showTz=1&amp;title"
           frameborder="0"
           scrolling="no"
         ></iframe>

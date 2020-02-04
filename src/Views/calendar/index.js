894686faede805543dd4e2646f3c76e5ca945629
ErickJoestar <erickborquez@gmail.com>
2020-02-03 18:37:10 -0600

    update calendar

--- src/Views/calendar/index.js
+++ src/Views/calendar/index.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+
 import Navigation from "../../Components/navigation";
+
+import { ReactComponent as Close } from "../../assets/close-icon.svg";
 
 import "./style.css";
 
 const Calendar = () => {
   const [events, setEvents] = useState([]);
+  const [box, setBox] = useState({ visible: false, x: 10, y: 0, content: {} });
 
   useEffect(() => {
     const start = () => {
@@ -18,31 +28,96 @@ const Calendar = () => {
           })
         )
         .then(
-          response => setEvents(response.result.items),
+          response => {
+            setEvents(
+              response.result.items
+                .filter(
+                  e =>
+                    e.status === "confirmed" &&
+                    e.start.dateTime &&
+                    e.description
+                )
+                .map((e, i) => ({
+                  ...e,
+                  date: e.start.dateTime.slice(0, 10)
+                }))
+            );
+          },
           error => console.error(error)
         );
     };
     window.gapi.load("client", start);
   }, []);
-  console.log(events);
+  const handleEvent = e => {
+    console.log(e.event);
+
+    setBox({
+      visible: true,
+      x: e.jsEvent.pageX - e.jsEvent.offsetX,
+      y: e.jsEvent.pageY - e.jsEvent.offsetY,
+      content: { title: e.event.title, ...e.event.extendedProps }
+    });
+  };
+  console.log(box);
 
   return (
     <>
       <Navigation selection="calendar" />
       <div className="cac_calendar">
-        <div className="cac_calendar_title">
-          Calendario Actividades Club de Algoritmia CUCEI
+        <div className="cac_calendar_container">
+          <div
+            className="cac_calendar_info-box"
+            style={{
+              top: box.y,
+              left: box.x,
+              opacity: box.visible ? "100%" : "0%",
+              zIndex: box.visible ? 10 : 0
+            }}
+          >
+            <Close
+              className="cac_calendar_info_close"
+              onClick={() => setBox({ ...box, visible: false })}
+            />
+            <span className="cac_calendar_info_title">{box.content.title}</span>
+
+            <span className="cac_calendar_info_location">
+              {box.content.location}
+            </span>
+
+            <span className="cac_calendar_info_description">
+              {box.content.description}
+            </span>
+          </div>
+          <FullCalendar
+            defaultView="dayGridMonth"
+            eventClick={handleEvent}
+            plugins={[dayGridPlugin, interactionPlugin]}
+            height="auto"
+            header={{
+              left: "title",
+              center: "prev today next",
+              right: ""
+            }}
+            buttonText={{
+              today: "Today"
+            }}
+            aspectRatio={2}
+            events={events.map(e => ({
+              title: e.summary,
+              date: e.date,
+              id: e.id,
+              className: "cac_calendar_event",
+              description: e.description,
+              location: e.location
+            }))}
+          />
         </div>
-        <iframe
-          title="calendar"
-          className="cac_calendar_calendar"
-          src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FMexico_City&amp;src=Y2x1Yi5hbGdvcml0bWlhLmN1Y2VpQGdtYWlsLmNvbQ&amp;color=%2322AA99&amp;mode=MONTH&amp;hl=en&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;showTz=1&amp;title"
-          frameborder="0"
-          scrolling="no"
-        ></iframe>
       </div>
     </>
   );
 };
-
+// events={[
+//   { title: "event 1", date: "2020-02-02" },
+//   { title: "event 2", date: "2020-02-03" }
+// ]}
 export default Calendar;

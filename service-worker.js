ced371d1ffaa864424b0e45d488630d0309cb64a
ErickJoestar <erickborquez@gmail.com>
2020-02-29 01:23:11 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b0a3342ba3f619b70f9861d58224fab4.js"
+  "/precache-manifest.2af9d7ccad366e1b4bf10d7f0a8ec10e.js"
 );
 
 self.addEventListener('message', (event) => {

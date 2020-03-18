d576b6a9d6325a909589b7371d93f949f81d8ba4
ErickJoestar <erickborquez@gmail.com>
2020-03-18 11:40:30 -0700

    update firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,48 @@
 const functions = require('firebase-functions');
 
+const config = require('../config');
+
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
+
+admin.initializeApp();
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: config.mail.user,
+    pass: config.mail.password
+  }
+})
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+
+    // getting dest email by query string
+    //const dest = req.query.dest;
+
+    const mailOptions = {
+      from: `Club de Algoritmia CUCEI <${config.mail.user}>`,
+      to: "erickborquez@gmail.com",
+      subject: 'I\'M A PICKLE!!!', // email subject
+      html: `<p style="font-size: 16px;">Pickle Riiiiiiiiiiiiiiiick!!</p>
+              <br />
+              <img src="https://images.prod.meredith.com/product/fc8754735c8a9b4aebb786278e7265a5/1538025388228/l/rick-and-morty-pickle-rick-sticker" />
+          ` // email content in HTML
+    };
+
+    // returning result
+    return transporter.sendMail(mailOptions, (erro, info) => {
+      if (erro) {
+        return res.send(erro.toString());
+      }
+      return res.send('Sended');
+    });
+  });
+});
+
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
